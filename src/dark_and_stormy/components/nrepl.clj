994cb8203dd84cb15227469b0e77ff842d3a56fc
Ian Truslove <ian.truslove@gmail.com>
2015-05-23 12:15:07 -0600

    Fixed record props that weren't

--- src/dark_and_stormy/components/nrepl.clj
+++ src/dark_and_stormy/components/nrepl.clj
@@ -11,21 +11,21 @@
       insert-cider-middleware ((fn [opts] (log/info "Enabling Cider middleware")
                                  (into opts [:handler cider-nrepl-handler]))))))
 
-(defrecord Nrepl [config]
+(defrecord Nrepl [config server]
   component/Lifecycle
   (start [this]
-    (if-not (:nrepl this)
+    (if-not (:server this)
       (let [nrepl-config (config/config config :nrepl)]
         (log/info "Starting nREPL on port" (:port nrepl-config))
-        (assoc this :nrepl (apply nrepl/start-server (nrepl-opts nrepl-config))))
+        (assoc this :server (apply nrepl/start-server (nrepl-opts nrepl-config))))
       (do
         (log/warn "Skipping starting nREPL - it's already started")
         this)))
 
   (stop [this]
-    (if (:nrepl this)
+    (if (:server this)
       (do (log/info "Stopping nREPL")
-          (update-in this [:nrepl] (fn [nrepl] (nrepl/stop-server nrepl)
+          (update-in this [:server] (fn [nrepl] (nrepl/stop-server nrepl)
                                      nil)))
       (do
         (log/warn "Skipping stopping nREPL - it's not running")
--- src/dark_and_stormy/components/webserver.clj
+++ src/dark_and_stormy/components/webserver.clj
@@ -4,26 +4,27 @@
             [dark-and-stormy.components.config :as config]
             [ring.adapter.jetty :as jetty]))
 
-(defn handler [req]
+(defn say-hi [req]
   {:status 200 :body "Hi"})
 
-(defrecord JettyWebserver [config]
+(defrecord JettyWebserver [config server handler]
   component/Lifecycle
   (start [this]
-    (if-not (:jetty this)
+    (if-not (:server this)
       (let [port (config/config config :webserver :port)]
         (log/info "Starting JettyWebserver on port" port)
-        (assoc this :jetty (jetty/run-jetty #'handler {:port port :join? false})))
+        (assoc this :server (jetty/run-jetty #'say-hi {:port port :join? false})))
       (do
         (log/warn "Skipping starting webserver - it's already started")
         this)))
 
   (stop [this]
-    (if (:jetty this)
+    (if (:server this)
       (do (log/info "Stopping JettyWebserver")
-          (update-in this [:jetty] (fn [jetty]
+          (update-in this [:server] (fn [jetty]
                                      (.stop jetty)
                                      nil)))
       (do
         (log/warn "Skipping stopping webserver - it's not running")
         this))))
+
