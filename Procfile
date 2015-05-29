015eb9d281d7dd85a79a3bbae36aa012804fb17a
Ian Truslove <ian.truslove@gmail.com>
2015-05-28 20:25:01 -0600

    Heroku setup

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JVM_OPTS -jar target/dark-and-stormy.jar
--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :resource-paths ["etc" "resources"]
+  :min-lein-version "2.0.0"
+  :uberjar-name "dark-and-stormy.jar"
+  :resource-paths ["resources"]
   :main dark-and-stormy.core
   :dependencies [[cider/cider-nrepl "0.8.2"]
                  [clj-http "1.1.2"]
@@ -24,5 +26,8 @@
                  [org.slf4j/slf4j-log4j12 "1.7.1"]
                  [ring "1.4.0-RC1"]
                  [sonian/carica "1.1.0"]]
-  :profiles {:dev {:global-vars {clojure.core/*warn-on-reflection* true}
-                   :plugins [[lein-deps-tree "0.1.2"]]}})
+  :profiles {:dev {:resource-paths ["etc/dev"]
+                   :global-vars {clojure.core/*warn-on-reflection* true}
+                   :plugins [[lein-deps-tree "0.1.2"]]}
+             :uberjar {:resource-paths ["etc/heroku"]
+                       :aot :all}})
